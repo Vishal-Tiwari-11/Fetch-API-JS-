c979405fdf56eff553e7cbf4e3745cb840474de5
Vishal Tiwari <140997042+Vishal-Tiwari-11@users.noreply.github.com>
2024-04-09 13:27:23 +0530

    Update script.js
    Fetch API by using Promises

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const URL= "https://cat-fact.herokuapp.com/facts"
 let factpara= document.querySelector("#para")
 let btn = document.querySelector("#btn")
 
+// ===========  async-await ===========
 const getFacts = async ()=>{
 console.log("Getting Data........");
     let response= await fetch (URL)
@@ -10,5 +11,17 @@ let data= await response.json();
 // console.log(data[0].text);
 factpara.innerText= data[1].text
 }
+
+// ========== promise ===========
+// function getFacts() {
+//     fetch(URL)
+//         .then((response) => {
+//             return response.json();
+//         })
+//         .then((data) => {
+//             console.log(data);
+//             factpara.innerText = data[3].text
+//         })
+// }
 
 btn.addEventListener("click", getFacts)
